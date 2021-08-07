478ae932325b61167b5e2aeac2b5c3b90b2b7383
GRAFSHEPARD\Admin <repnau.jankristjan@gmail.com>
2021-08-07 13:27:01 +0300

    Fields in Evaluation, gym, gymHasMembership and members

--- src/main/java/model/Evaluation.java
+++ src/main/java/model/Evaluation.java
@@ -1,4 +1,18 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Evaluation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "evaluation_id")
+    private int evaluationId;
+
+    private int rating;
+
+    private String comment;
+
+
 }
--- src/main/java/model/GymHasMembership.java
+++ src/main/java/model/GymHasMembership.java
@@ -1,4 +1,19 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class GymHasMembership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gym_id")
+    private int gymId;
+
+    @Column(name = "membership_id")
+    private int membershipId;
+
+
 }
