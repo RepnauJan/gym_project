1e1193f14afb57d2b7807883ca1fcfd58b4f7211
Ragnar Laak <ragnarlaak@gmail.com>
2021-08-07 14:06:52 +0300

    Filled in the fields for the model classes plus added raltionship between gym table and membership table

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,4 +1,24 @@
 package model;
 
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "transaction_id")
+    private int transactionId;
+
+    @Column(name = "amount")
+    private float amount;
+
+    @Column(name = "payment_date")
+    private Date paymentDate;
+
+    @Column(name = "member_id")
+    private int memberId;
 }
