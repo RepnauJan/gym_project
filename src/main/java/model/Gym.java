c59784c69fb4af9ff4f2515a93a5eca1955e1f83
GRAFSHEPARD\Admin <repnau.jankristjan@gmail.com>
2021-08-07 13:51:19 +0300

    Relationships in Evaluation, gym, gymHasMembership and members. Getters and setters and toString

--- src/main/java/model/Gym.java
+++ src/main/java/model/Gym.java
@@ -1,9 +1,8 @@
 package model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Gym {
 
@@ -19,5 +18,66 @@ public class Gym {
     @Column(name = "membership_id")
     private int membershipId;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
 
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "gym_has_membership",
+            joinColumns = { @JoinColumn(name = "gym_id") },
+            inverseJoinColumns = { @JoinColumn(name = "membership_id") }
+    )
+    Set<Membership> projects = new HashSet<>();
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(int membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    @Override
+    public String toString() {
+        return "Gym{" +
+                "gymId=" + gymId +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", membershipId=" + membershipId +
+                ", schedule=" + schedule +
+                '}';
+    }
 }
